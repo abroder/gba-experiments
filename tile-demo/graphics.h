b14fa8119ee0810deb29e73b43b24e47cd545e4b
Aaron Broder <aaronbroder@gmail.com>
2015-09-04 21:48:09 +0100

    Moved graphics to graphics file.

---
+++ tile-demo/graphics.h
@@ -0,0 +1,8 @@
+#define palleteLen 10
+extern const unsigned short pallete[5];
+
+#define tilesLen 224
+extern const unsigned short tiles[112];
+
+#define mapLen 2048
+extern const unsigned short map[1024];
